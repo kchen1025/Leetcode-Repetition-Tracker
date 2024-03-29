106c8c4221fc18c2fb66aadc64787618d0c0abc5
Kevin Chen <kchen1025@gmail.com>
2024-03-29 15:56:21 -0700

    test

--- models/pool.js
+++ models/pool.js
@@ -1,12 +1,12 @@
 import { Pool } from "pg";
 require("dotenv").config();
 
-let ssl = {};
-if (process.env.NODE_ENV === "production") {
-  ssl = {
-    rejectUnauthorized: false,
-  };
-}
+// let ssl = {};
+// if (process.env.NODE_ENV === "production") {
+//   ssl = {
+//     rejectUnauthorized: false,
+//   };
+// }
 
 // refresher on how to connect,
 // I use pg-cli for all direct database connections and elephantsql for easy instances setup
@@ -15,5 +15,7 @@ if (process.env.NODE_ENV === "production") {
 // pgcli -h <dbURL> -U <DBUSER> -d <DBNAME>'
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl,
+  ssl: {
+    rejectUnauthorized: false, // Note: Setting this to false can create security vulnerabilities
+  },
 });
