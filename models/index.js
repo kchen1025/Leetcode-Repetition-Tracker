1bbba3dcf384b42ecd6f1f03794aac287701d674
Kevin Chen <kchen1025@gmail.com>
2024-03-29 16:17:21 -0700

    update sequelize

--- models/index.js
+++ models/index.js
@@ -6,7 +6,21 @@ dotenv.config();
 const db = {};
 
 const initDb = () => {
-  const sequelize = new Sequelize(process.env.DATABASE_URL || "");
+  const sequelize = new Sequelize(process.env.DATABASE_URL || "", {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Note: Setting this to false can introduce security vulnerabilities.
+      },
+    },
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  });
   (async () => {
     try {
       await sequelize.authenticate();
